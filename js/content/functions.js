e926c58b35f9623dd67ee31b8d641a2db7053675
agusnurwanto <agusnurwantomuslim@gmail.com>
2023-01-15 08:50:26 +0700

    perbaikan exponents volume desimal

--- js/content/functions.js
+++ js/content/functions.js
@@ -5574,7 +5574,27 @@ function get_master_rekening_rka(idrkpdrenjaaktivitas, idssh_4){
 	});
 }
 
+Number.prototype.noExponents = function() {
+  	var data = String(this).split(/[eE]/);
+  	if (data.length == 1) return data[0];
+
+  	var z = '',
+	    sign = this < 0 ? '-' : '',
+	    str = data[0].replace('.', ''),
+	    mag = Number(data[1]) + 1;
+
+  	if (mag < 0) {
+	    z = sign + '0.';
+	    while (mag++) z += '0';
+	    return z + str.replace(/^\-/, '');
+  	}
+	mag -= str.length;
+	while (mag--) z += '0';
+	return str + z;
+}
+
 function replace_number(number){
+	number = (+number).noExponents();
 	if(_type_singkronisasi_rka == 'rka-opd'){
 		return (number+'').replace(/\./g, ',');
 	}else{
